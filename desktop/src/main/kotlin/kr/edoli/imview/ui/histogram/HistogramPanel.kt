c7d9cb184bc79ebc57a78f4689e37ad664dc388c
Daniel <edoli.mia@gmail.com>
2019-09-07 11:48:26 +0900

    Histogram

--- desktop/src/main/kotlin/kr/edoli/imview/ui/histogram/HistogramPanel.kt
+++ desktop/src/main/kotlin/kr/edoli/imview/ui/histogram/HistogramPanel.kt
@@ -1,17 +1,67 @@
-package kr.edoli.imview.ui
+package kr.edoli.imview.ui.histogram
 
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.ui.Slider
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import kr.edoli.imview.ImContext
+import kr.edoli.imview.image.split
+import kr.edoli.imview.ui.Panel
+import kr.edoli.imview.ui.UIFactory
 import kr.edoli.imview.util.Histogram
-import org.opencv.core.Core
 
 class HistogramPanel : Panel(false) {
-    val histograms = ArrayList<Histogram>()
+
+    val histogramActor = HistogramWidget()
+    val buttons = Table()
+    val slider = Slider(1f, 255f, 1f, false, UIFactory.skin).apply {
+        value = 100f
+        addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent, actor: Actor) {
+                updateHistogram()
+            }
+        })
+    }
 
     init {
-        ImContext.mainImage.subscribe { mat ->
-            if (mat == null) return@subscribe
+        add(histogramActor).expandX().fillX().height(128f)
+        row()
+        add(buttons).expandX().fillX()
+        row()
+        add(slider).expandX().fillX()
 
+        ImContext.mainImage.subscribe { updateHistogram() }
+    }
 
+    fun updateHistogram() {
+        val mat = ImContext.mainImage.get() ?: return
+
+        val binSize = slider.value
+
+        val histograms = mat.split().map { singleMat ->
+            Histogram(binSize.toInt(), (1 / (binSize - 1)).toDouble()).apply {
+                computeHistMat(singleMat)
+            }
+        }
+
+        histogramActor.histograms.clear()
+        histogramActor.histograms.addAll(histograms)
+
+        buttons.clearChildren()
+        val names = when (mat.channels()) {
+            1 -> arrayOf("Gray")
+            3 -> arrayOf("Red", "Green", "Blue")
+            4 -> arrayOf("Red", "Green", "Blue", "Alpha")
+            else -> arrayOf()
+        }
+
+        (names.indices).forEach { index ->
+            buttons.add(UIFactory.createTextButton(names[index]) {
+                histograms[index].isShow = it.isChecked
+            }.apply {
+                isChecked = true
+                style = UIFactory.textToggleButtonStyle
+            })
         }
     }
 }
--- desktop/src/main/kotlin/kr/edoli/imview/ui/histogram/HistogramWidget.kt
+++ desktop/src/main/kotlin/kr/edoli/imview/ui/histogram/HistogramWidget.kt
@@ -2,22 +2,24 @@ package kr.edoli.imview.ui.histogram
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
-import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.ui.Widget
 import kr.edoli.imview.ui.ColorRect
 import kr.edoli.imview.util.Histogram
 
-class HistogramActor : Actor() {
+class HistogramWidget : Widget() {
     val histograms = ArrayList<Histogram>()
     val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.GRAY)
 
     override fun draw(batch: Batch, parentAlpha: Float) {
+        super.draw(batch, parentAlpha)
+
         histograms.forEachIndexed { histIndex, hist ->
             if (!hist.isShow) {
                 return@forEachIndexed
             }
 
             val num = hist.n
-            val barWidth = width / (num + 4)
+            val barWidth = width / num
             val freq = hist.freq
 
             batch.color = colors[histIndex]
