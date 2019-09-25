686b5bd569e143c2a3fca5a21bd3a26cead9177f
Daniel Jeon <edoli.mia@gmail.com>
2019-09-25 16:43:28 +0900

    Change import

--- desktop/src/main/kotlin/kr/edoli/imview/ui/ImageViewer.kt
+++ desktop/src/main/kotlin/kr/edoli/imview/ui/ImageViewer.kt
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
