0c596c3ea07488704781f06d7dbdfb0fbb5d701d
Daniel S. Jeon <edoli.mia@gmail.com>
2020-12-11 18:24:44 +0900

    Function time util

--- desktop/src/main/kotlin/kr/edoli/imview/util/Utils.kt
+++ desktop/src/main/kotlin/kr/edoli/imview/util/Utils.kt
@@ -4,3 +4,10 @@ import java.util.UUID
 
 
 fun generateId(): Long { return UUID.randomUUID().mostSignificantBits and java.lang.Long.MAX_VALUE }
+
+fun <T> functionTime(func: () -> T): T {
+    val startTime = System.nanoTime()
+    val value = func()
+    println("${(System.nanoTime() - startTime).toFloat() / 1000 / 1000}ms")
+    return value
+}
