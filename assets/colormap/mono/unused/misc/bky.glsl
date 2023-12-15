321bc5be7725b8becbe71982f0340668ef653131
Donggun KIM <dgkim1003@kaist.ac.kr>
2023-12-15 17:45:55 +0900

    colormaps

---
+++ assets/colormap/mono/unused/misc/bky.glsl
@@ -0,0 +1,12 @@
+// Blue-Black-Yellow colormap
+
+vec3 bky_colormap(float t) {
+    vec3 low_color = vec3(0.15, 0.15, 1.0);
+    vec3 high_color = vec3(1.0, 1.0, 0.15);
+
+    float interp = abs(1.0 - 2.0 * t); // 0 ~ 1 ~ 0
+
+    vec3 end_color = t < 0.5 ? low_color : high_color;
+    vec3 rgb = end_color * interp;
+    return clamp(rgb, 0.0, 1.0);
+}
---
+++ assets/colormap/mono/unused/misc/gkr.glsl
@@ -0,0 +1,13 @@
+// Green-Black-Red colormap
+// Note that the end color is not the exact green (0, 1, 0) and red (1, 0, 0)
+
+vec3 gkr_colormap(float t) {
+    vec3 low_color = vec3(0.15, 1.0, 0.15);
+    vec3 high_color = vec3(1.0, 0.15, 0.15);
+
+    float interp = abs(1.0 - 2.0 * t); // 0 ~ 1 ~ 0
+
+    vec3 end_color = t < 0.5 ? low_color : high_color;
+    vec3 rgb = end_color * interp;
+    return clamp(rgb, 0.0, 1.0);
+}
