54401586cb416c3619fcb0d2c5424532306f384b
edoli <edoli.mia@gmail.com>
2025-06-20 09:02:59 +0900

    Fix not use win32 api to all platforms

--- desktop/src/main/java/kr/edoli/edolview/WInit.java
+++ desktop/src/main/java/kr/edoli/edolview/WInit.java
@@ -1,18 +1,11 @@
 package kr.edoli.edolview;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Window;
-import org.lwjgl.glfw.GLFWNativeWin32;
 import org.lwjgl.system.*;
 
 public final class WInit {
     public static void winit(Lwjgl3Window window) {
-        long hwnd = GLFWNativeWin32.glfwGetWin32Window(window.getWindowHandle());
-        if (hwnd == 0L) {
-            System.err.println("Failed to get foreground HWND.");
-            return;
-        }
 
-        // If OS is Windows, set the DWM attributes
         if (Platform.get() == Platform.WINDOWS) {
             Windows.winit(window);
         }
